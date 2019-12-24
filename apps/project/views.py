9fe0fb3e6cf0386bcd036aa0555147936859d89d
h4zzkR <tramvenus@yandex.ru>
2019-12-24 20:14:11 +0300

    context breaks form fields

--- apps/project/views.py
+++ apps/project/views.py
@@ -31,10 +31,10 @@ class ProjectCreate(FormView):
         kwargs['user'] = self.request.user
         return kwargs
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context = get_context(self.request, 'Новый проект')
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context = get_context(self.request, 'Новый проект')
+    #     return context
 
     def form_valid(self, form):
         form.save()
