e9781118b752ef8a822b7a25252b8c7de1aae123
Ovchinnikov Vsevolod <sevavov.2004@gmail.com>
2020-02-22 21:02:40 +0300

    Trying to fix DEBUG param heroku

--- alpha/settings.py
+++ alpha/settings.py
@@ -17,7 +17,7 @@ HOST = 'http://127.0.0.1:8000/'
 DOMAIN = 'concat.org'
 
 SECRET_KEY = '%vkruwrhxujcdcup=mnk_x1loax+8=4+$@(f2on5l$^hw-yl#_'
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 ALLOWED_HOSTS = ["*"]
 
 
@@ -148,10 +148,6 @@ if DEBUG:
                 'PASSWORD': 's1XTPKlOjWo4',
                 'HOST': 'localhost',
                 'PORT': '5432',
-                'OPTIONS': {
-                    'init_command': "SET sql_mode='STRICT_ALL_TABLES'",
-                    'charset': 'utf8mb4',
-                },
 
             }
         }
