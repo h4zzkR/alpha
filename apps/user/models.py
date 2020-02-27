b58127aa58fcf93a386419793f3043aba1b44447
h4zzkR <tramvenus@yandex.ru>
2020-02-27 22:33:30 +0300

    avata

--- apps/user/models.py
+++ apps/user/models.py
@@ -83,8 +83,10 @@ class UserProfile(models.Model):
         else:
             return False
 
+    @property
     def avatar_url(self):
-        return self.avatar.url
+        if self.avatar and hasattr(self.avatar, 'url'):
+            return self.image.url
 
     def email_user(self, subject, arguments, text_content=None, message=None):
         # arguments = { KEY : VALUE } for template
